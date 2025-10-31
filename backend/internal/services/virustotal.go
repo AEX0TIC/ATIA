8d23fcdfad847ae8fa74edde89007d0f35b18d47
AEX0TIC <prkhprt001@gmail.com>
2025-10-31 23:03:41 +0530

    Adding the new code with some important changes

--- backend/internal/services/virustotal.go
+++ backend/internal/services/virustotal.go
@@ -4,13 +4,43 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"io"
 	"time"
 )
 
 type VirusTotalService struct {
-	apiKey string 
+	apiKey string
 	client *http.Client
 }
 
-func NewVirusTotalService
+func NewVirusTotalService(apiKey string) *VirusTotalService {
+	return &VirusTotalService{
+		apiKey: apiKey,
+		client: &http.Client{Timeout: 30 * time.Second},
+	}
+}
+
+func (v *VirusTotalService) AnalyzeIP(ip string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://www.virustotal.com/api/v3/ip_addresses/%s", ip)
+	return v.makeRequest(url)
+}
+
+func (v *VirusTotalService) AnalyzeDomain(domain string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://www.virustotal.com/api/v3/ip_addresses/%s", ip)
+	return v.makeRequest(url)
+}
+
+func (v *VirusTotalService) AnalyzeHash(hash string) (map[string]interface{}, error) {
+	url := fmt.Sprintf("https://www.virustotal.com/api/v3/ip_addresses/%s", ip)
+	return v.makeRequest(url)
+}
+
+func (v *VirusTotalService) makeRequest(url string) (map[string]interface{}, error) {
+	if v.apiKey == "" {
+		return map[string]interface{"error": "API key not configured"}, nil
+	}
+	
+	
+
+
+
+}
