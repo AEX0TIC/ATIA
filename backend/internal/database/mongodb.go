98adbbc9feab81a15e4caf9d93deb8728d244791
AEX0TIC <prkhprt001@gmail.com>
2025-10-31 22:44:49 +0530

    Updated the mongoDB connection and operation code

--- backend/internal/database/mongodb.go
+++ backend/internal/database/mongodb.go
@@ -2,17 +2,20 @@ package database
 
 import (
 	"context"
-	"log"
 	"time"
 
-	"github.com/AEX0TIC/ATIA/backend/internal/models"
+	"atia/internal/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoDB struct {
-	client   *mongo.Client
-	database *mongo.Database
+	client     *mongo.Client
+	database   *mongo.Database
+	collection *mongo.Collection
 }
 
 func NewMongoDB(uri, dbName string) (*MongoDB, error) {
@@ -25,61 +28,112 @@ func NewMongoDB(uri, dbName string) (*MongoDB, error) {
 	}
 
 	// Ping the database
-	err = client.Ping(ctx, nil)
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+
+	db := client.Database(dbName)
+	collection := db.Collection("threats")
+
+	// Create indexes
+	indexModel := mongo.IndexModel{
+		Keys:    bson.D{{Key: "indicator", Value: 1}},
+		Options: options.Index().SetUnique(true),
+	}
+	_, err = collection.Indexes().CreateOne(ctx, indexModel)
 	if err != nil {
 		return nil, err
 	}
 
-	database := client.Database(dbName)
 	return &MongoDB{
-		client:   client,
-		database: database,
+		client:     client,
+		database:   db,
+		collection: collection,
 	}, nil
 }
 
-func (db *MongoDB) CreateIndexes() error {
-	ctx := context.Background()
+func (m *MongoDB) SaveThreat(threat *models.ThreatIndicator) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	// Create indexes for the threats collection
-	threatsCollection := db.database.Collection("threats")
-
-	indexes := []mongo.IndexModel{
-		{
-			Keys: map[string]interface{}{
-				"indicator": 1,
-			},
-			Options: options.Index().SetUnique(true),
-		},
-		{
-			Keys: map[string]interface{}{
-				"type": 1,
-			},
-		},
-		{
-			Keys: map[string]interface{}{
-				"score": 1,
-			},
-		},
+	threat.LastUpdated = time.Now()
+	if threat.FirstSeen.IsZero() {
+		threat.FirstSeen = time.Now()
 	}
 
-	_, err := threatsCollection.Indexes().CreateMany(ctx, indexes)
-	if err != nil {
-		log.Printf("Error creating indexes: %v", err)
-		return err
-	}
+	filter := bson.M{"indicator": threat.Indicator}
+	update := bson.M{"$set": threat}
+	opts := options.Update().SetUpsert(true)
 
-	return nil
-}
-
-func (db *MongoDB) InsertThreat(threat *models.Threat) error {
-	collection := db.database.Collection("threats")
-	ctx := context.Background()
-
-	_, err := collection.InsertOne(ctx, threat)
+	_, err := m.collection.UpdateOne(ctx, filter, update, opts)
 	return err
 }
 
-func (db *MongoDB) Close() error {
-	ctx := context.Background()
-	return db.client.Disconnect(ctx)
+func (m *MongoDB) GetThreat(indicator string) (*models.ThreatIndicator, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var threat models.ThreatIndicator
+	err := m.collection.FindOne(ctx, bson.M{"indicator": indicator}).Decode(&threat)
+	if err != nil {
+		return nil, err
+	}
+
+	return &threat, nil
+}
+
+func (m *MongoDB) GetAllThreats(limit int64) ([]models.ThreatIndicator, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	opts := options.Find().SetLimit(limit).SetSort(bson.D{{Key: "last_updated", Value: -1}})
+	cursor, err := m.collection.Find(ctx, bson.M{}, opts)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var threats []models.ThreatIndicator
+	if err := cursor.All(ctx, &threats); err != nil {
+		return nil, err
+	}
+
+	return threats, nil
+}
+
+func (m *MongoDB) GetThreatHistory(indicator string) ([]models.ThreatIndicator, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := m.collection.Find(ctx, bson.M{"indicator": indicator})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var history []models.ThreatIndicator
+	if err := cursor.All(ctx, &history); err != nil {
+		return nil, err
+	}
+
+	return history, nil
+}
+
+func (m *MongoDB) DeleteThreat(id string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = m.collection.DeleteOne(ctx, bson.M{"_id": objID})
+	return err
+}
+
+func (m *MongoDB) Disconnect() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return m.client.Disconnect(ctx)
 }
